ffa5bd35f4af026996a3d20b79b611bce6cd47d8
Arnel Ambrosio <arnelfambrosio@gmail.com>
2020-06-09 17:41:36 +0800

    -added web mvc core app

---
+++ Ustockit.Uploader/Ustockit.Uploader.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Ustockit.Uploader.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
